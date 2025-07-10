b118b6ca071362b3fc6ead1228ee78c466b26d38
Matthieu Jorel <matthieu.jorel@iut-dijon.u-bourgogne.fr>
2025-07-10 11:22:51 +0200

    View layer management

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
-    require_once('Psr4AutoloaderClass.php');
-
     ini_set('display_errors', '1');
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
 
+    require_once('Helpers/Psr4AutoloaderClass.php');
+
     $loader = new Helpers\Psr4AutoloaderClass();
     $loader->register();
-?>
+    $loader->addNamespace('\Helpers', '/Helpers');
+    $loader->addNamespace('League\Plates', 'Vendor/Plates/src');
+    $templates = new League\Plates\Engine('Views');
+
+    echo $templates->render('home', ['tftSetName' => 'Test']);
+    ?>
